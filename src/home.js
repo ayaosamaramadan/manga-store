3aec5f7eb2e08eeb2814de08c5c44bd15b97ee66
ayaosamaramadan <ayaramadan2011@gmail.com>
2025-01-25 07:04:46 +0200

    style home page

--- src/home.js
+++ src/home.js
@@ -1,13 +1,7 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     const loggedInUserKey = localStorage.getItem('loggedInUser');
-//     if (!loggedInUserKey) {
-//         // If no user is logged in, redirect to login page
-//         window.location.href = './index.html'; // Ensure the path is correct
-//     } else {
-//         // Retrieve and log user information
-//         window.location.href = './home.html';
-//         const user = JSON.parse(localStorage.getItem(loggedInUserKey));
-//         console.log('Email:', user.email);
-//         console.log('Password:', user.password);
-//     }
-// });
+const data = [
+    {name:"", price: 0 ,Image:"",description:""},
+    {name:"", price: 0 ,Image:"",description:""},
+    {name:"", price: 0 ,Image:"",description:""},
+];
+
+// console.log("data", data);
