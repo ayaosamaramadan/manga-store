2ccf772010c04f34dff467b5a8315a4eaaa5f5f0
ayaosamaramadan <ayaramadan2011@gmail.com>
2025-01-26 22:09:14 +0200

    remove selected cartItem

--- src/cartitems.js
+++ src/cartitems.js
@@ -1,28 +1,38 @@
 let totalpriceDiv = document.getElementById('total-p');
+let removeCart = document.getElementById('remove-cart');
 let totalPrice = 0;
+
 document.addEventListener('DOMContentLoaded', function() {
     const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
     const cartitemsdiv = document.getElementById('cart-items');
-
-
         cartItems.forEach(cartItem => {
             const mangaincart = document.createElement('div');
             mangaincart.classList.add('cartstyle');
-            // mangaincart.style.cssText = "margin: 4%; padding: 10px;";
             mangaincart.innerHTML = `
                 <img class="cartItem-img" src="${cartItem.images.jpg.image_url}" alt="${cartItem.title}">
                 <div class="cartItem-info">
-                <h3 class='cartItem-title'>${cartItem.title}</h3>
-                <br>   
-
-                <p class="cartItem-price text-wheat">$${cartItem.score}</p>
+                 <i class="fa-solid fa-x remove-cart"></i>
+                 <h3 class='cartItem-title'>${cartItem.title}</h3>
+                 <p class="cartItem-price text-wheat">$${cartItem.score}</p>
             </div>
                 `;
+            const removeButton = mangaincart.querySelector('.remove-cart');
+            removeButton.addEventListener('click', () => removecart(cartItem));
             totalPrice += cartItem.score;
             totalpriceDiv.innerHTML = `TOTAL PRICE : $ ${totalPrice.toFixed(2)}`;
             cartitemsdiv.appendChild(mangaincart);
+
+            
         });
     
 });
 
 (totalPrice===0)?totalpriceDiv.innerHTML = `TOTAL PRICE : $ ${totalPrice}`:totalpriceDiv.innerHTML = `TOTAL PRICE : $ ${totalPrice}`;
+
+
+const removecart = (cartItem) => {  
+    const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+    const newcartitems = cartItems.filter(item => item.mal_id !== cartItem.mal_id);
+    localStorage.setItem('cartItems', JSON.stringify(newcartitems));
+    location.reload();
+}
